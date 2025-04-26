3012b5eda0cdca15cc24d108da966431aa357840
Leonardo Bonati <leonardo.bonati@gmail.com>
2025-04-26 14:40:48 -0400

    About

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Colosseum'
+copyright = '2025, Colosseum @ Northeastern University WIoT'
+author = 'Colosseum'
 
 release = '0.1'
 version = '0.1.0'
