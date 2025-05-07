2764d38c7c7765491edfed4d700202313ba04437
Davide Villa <villa.d@northeastern.edu>
2025-05-06 23:47:54 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,6 @@ templates_path = ['_templates']
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 
-# -- Logo
-
-# html_logo = "_static/resources/index/logo.png"
-
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
